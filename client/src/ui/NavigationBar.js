679ddd8f8fd8e8c0188c30c8d83d86c83b1c22ba
Eric Kudler <joeento@gmail.com>
2020-04-02 00:00:24 -0400

    Adds navbar to page

---
+++ client/src/ui/NavigationBar.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+//import { withRouter } from 'react-router';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import { FaTorah } from 'react-icons/fa';
+
+import './NavigationBar.css';
+
+class NavigationBar extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <Navbar bg="black" expand="lg" variant="dark" >
+        <Navbar.Brand href="#">
+          <FaTorah className='mr-2' size="1.6em" />HaggaDOS
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav" >
+          <Nav className="ml-auto" activeKey={false}>
+            <Nav.Link>About</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    );
+  }
+}
+
+export default NavigationBar;
